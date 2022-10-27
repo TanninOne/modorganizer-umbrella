466bdb0d5efde8b8f48b3a32b59b729a062b9546
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 05:09:46 +0000

    Adding tarfile member sanitization to extractall()

--- unibuild/modules/urldownload.py
+++ unibuild/modules/urldownload.py
@@ -128,12 +128,50 @@ class URLDownload(Retrieval):
             if extension == ".gz" or extension == ".tgz":
                 archive_file = ProgressFile(archive_file_path, progress_func)
                 with tarfile.open(fileobj=archive_file, mode='r:gz') as arch:
-                    arch.extractall(output_file_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(arch, output_file_path)
                 archive_file.close()
             elif extension == ".bz2":
                 archive_file = ProgressFile(archive_file_path, progress_func)
                 with tarfile.open(fileobj=archive_file, mode='r:bz2') as arch:
-                    arch.extractall(output_file_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(arch, output_file_path)
                 archive_file.close()
             elif extension == ".zip":
                 archive_file = ProgressFile(archive_file_path, progress_func)
